346530e38d3f8c512c9acddf40d64cd31ef0aab6
Julian Seidenberg <julian@silverstripe.com>
2015-12-21 14:38:10 +1300

    NEW filling out the unit tests

--- tests/WhitelistGeneratorTest.php
+++ tests/WhitelistGeneratorTest.php
@@ -4,9 +4,7 @@ class WhitelistGeneratorTest extends SapphireTest{
 	protected static $fixture_file = 'WhitelistTest.yml';
 	
 	function testGenerateWhitelist(){
-
 		$whitelist = WhitelistGenerator::generateWhitelistRules();
-		Debug::Show($whitelist);
 
 		$top1 = $this->objFromFixture('SiteTree', 'top1');
 		$top2 = $this->objFromFixture('SiteTree', 'top2');
@@ -17,8 +15,22 @@ class WhitelistGeneratorTest extends SapphireTest{
 		$child4 = $this->objFromFixture('SiteTree', 'childchild2');
 		$child5 = $this->objFromFixture('SiteTree', 'childchildchild1');
 		
+		$this->assertContains(trim($top1->relativeLink(),'/'), $whitelist);
+		$this->assertContains(trim($top2->relativeLink(),'/'), $whitelist);
+		$this->assertContains(trim($top3->relativeLink(),'/'), $whitelist);
+		$this->assertNotContains(trim($child1->relativeLink(),'/'), $whitelist);
+		$this->assertNotContains(trim($child2->relativeLink(),'/'), $whitelist);
+		$this->assertNotContains(trim($child3->relativeLink(),'/'), $whitelist);
+		$this->assertNotContains(trim($child4->relativeLink(),'/'), $whitelist);
+		$this->assertNotContains(trim($child5->relativeLink(),'/'), $whitelist);
+
+	}
+	
+	function testCustomControllerWhitelist() {
+		$whitelist = WhitelistGenerator::generateWhitelistRules();
 		
-		
+		//test that custom class defined below is included in the whitelist
+		$this->assertContains('WhitelistTestController', $whitelist);
 	}
 	
 }
